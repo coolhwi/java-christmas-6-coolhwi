5a3ab987aa339dca9c796b44a18ccd52d3e79023
DongHwi Kim <ken7905@ajou.ac.kr>
2023-11-12 23:26:32 +0900

    feat: 날짜 음식 Input 기능

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -3,15 +3,21 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-/*
-안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.
-12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)
-3
-주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)
-티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1
-12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!
-*/
 public class InputView {
+    private static final String FIRST_LINE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String INPUT_VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String INPUT_FOOD_MENU_AND_NUM = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
+    public static int enterVisitDate(){
+        System.out.println(FIRST_LINE);
+        System.out.println(INPUT_VISIT_DATE);
+        return Integer.parseInt(Console.readLine());
+    }
+
+    public static String enterFoodAndNum(){
+        System.out.println(INPUT_FOOD_MENU_AND_NUM);
+        return Console.readLine();
+    }
 
 
 }
